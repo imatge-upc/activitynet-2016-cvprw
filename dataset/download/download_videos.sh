9204ab0e2c110e4a61e5077f9ea08bdac8932404
Alberto Montes <alm59321@gmail.com>
2016-05-05 17:42:01 +0200

    Add a progressbar and update one file

--- dataset/download/download_videos.sh
+++ dataset/download/download_videos.sh
@@ -1,6 +1,7 @@
 list=$1
 
-youtube-dl -i -a $list \
+srun youtube-dl -i -a $list \
 -o '/imatge/amontes/work/datasets/ActivityNet/v1.3/videos/%(id)s.%(ext)s' \
 -f bestvideo[ext=mp4] \
--u al.montes.gomez@gmail.com
+-u al.montes.gomez@gmail.com \
+-p 321+creatividad!5115 > download.log 2>&1
--- work/tools/get_nb_frames.py
+++ work/tools/get_nb_frames.py
@@ -1,9 +1,11 @@
 import json
 import sys
 
-import cv2
+from progressbar import ProgressBar
 
-DOWNLOAD_PATH = '/imatge/amontes/work/datasets/ActivityNet/v1.3/videos'
+import cv2
+from work.config.config import STORED_VIDEOS_PATH
+
 
 def get_number_of_frames(video_path):
     """ Returns the number of frames of the video stored at the path given
@@ -20,11 +22,19 @@ def add_nb_frames_dataset(input_file, output_file):
     with open(input_file, 'r') as f:
         dataset = json.load(f)
 
+    progbar = ProgressBar(max_value=len(dataset['database'].keys()))
+    count = 0
+    progbar.update(count)
     for video_id in dataset['database'].keys():
-        nb_frames = get_number_of_frames(DOWNLOAD_PATH+'/'+video_id+'.mp4')
+        nb_frames = get_number_of_frames(STORED_VIDEOS_PATH+'/'+video_id+'.mp4')
         dataset['database'][video_id].update({
             'num_frames': nb_frames
         })
+        count += 1
+        if count % 100 == 0:
+            progbar.update(count)
+
+    progbar.finish()
 
     with open(output_file, 'w') as f:
         json.dump(dataset, f)
