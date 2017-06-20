d78e4a521dbc2c1957f505fba9bc7d6d073d2744
Alberto Montes <alm59321@gmail.com>
2017-06-20 08:28:07 +0200

    Prettify code

--- src/processing.py
+++ src/processing.py
@@ -10,19 +10,21 @@ def get_classification(sequence_class_prob, k=3):
     scores = class_prob[labels_index] / np.sum(class_prob[1:])
     return labels_index[:k], scores[:k]
 
+
 def smoothing(x, k=5):
     ''' Applies a mean filter to an input sequence. The k value specifies the window
     size. window size = 2*k
     '''
     l = len(x)
-    s = np.arange(-k, l-k)
-    e = np.arange(k, l+k)
-    s[s<0] = 0
-    e[e>=l] = l-1
+    s = np.arange(-k, l - k)
+    e = np.arange(k, l + k)
+    s[s < 0] = 0
+    e[e >= l] = l - 1
     y = np.zeros(x.shape)
     for i in range(l):
         y[i] = np.mean(x[s[i]:e[i]], axis=0)
     return y
+
 
 def activity_localization(sequence_class_prob, activity_threshold=.2):
     ''' From predicted classes probability return a sequence of temporal intervals
@@ -30,17 +32,17 @@ def activity_localization(sequence_class_prob, activity_threshold=.2):
     activity_idx, _ = get_classification(sequence_class_prob)
     activity_idx = activity_idx[0]
 
-    activity_prob = 1 - sequence_class_prob[:,0]
+    activity_prob = 1 - sequence_class_prob[:, 0]
     activity_tag = np.zeros(activity_prob.shape)
-    activity_tag[activity_prob>=activity_threshold] = 1
+    activity_tag[activity_prob >= activity_threshold] = 1
 
     assert activity_tag.ndim == 1
     padded = np.pad(activity_tag, pad_width=1, mode='constant')
-    dif = padded[1:] - padded [:-1]
+    dif = padded[1:] - padded[:-1]
 
     indexes = np.arange(dif.size).astype(np.float32)
-    startings = indexes[dif==1]
-    endings = indexes[dif==-1]
+    startings = indexes[dif == 1]
+    endings = indexes[dif == -1]
 
     assert startings.size == endings.size
 
@@ -48,8 +50,8 @@ def activity_localization(sequence_class_prob, activity_threshold=.2):
     scores = []
 
     for segment in zip(startings, endings):
-    	s, e = map(int, segment)
+        s, e = map(int, segment)
         activities_idx.append(activity_idx)
-        scores.append(np.mean(sequence_class_prob[s:e,activity_idx]))
+        scores.append(np.mean(sequence_class_prob[s:e, activity_idx]))
 
     return activities_idx, startings, endings, scores
